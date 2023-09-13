655b4951c44f0d8218526dd020baa5d59749a80c
Stanley <stanleyosazemwinde01@gmail.com>
2023-09-13 13:38:58 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,12 @@
 
 /**
  * print_alphabet - this is the function of the task
- * Return: return 0
  */
+
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
