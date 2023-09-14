29b42e3b4a21ac04fd4542819346e58c6b44aea9
Stanley <stanleyosazemwinde01@gmail.com>
2023-09-14 09:35:20 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,13 +2,13 @@
 
 /**
  * more_numbers -print numbers from 0-14 10 times
- * Return:void
+ * Return: void
  */
 
 void more_numbers(void)
 {
 
-	int i, j;
+int i, j;
 
 	for (i = 1; i <= 10; i++)
 	{
