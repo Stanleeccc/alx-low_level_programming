2248f96521d9b33315fcf5d8c1ed0a329688f1fb
Stanley <stanleyosazemwinde01@gmail.com>
2023-09-14 08:30:06 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mu1 - multiplies two factors
+ * @a: - The first integer
+ * @b: The second integer
+ * Return: Always 0
+ */
+
+int mu1(int a, int b)
+{
+int result = a * b;
+
+return (result);
+}
