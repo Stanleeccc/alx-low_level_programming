6bdcb06cb7fd6cfcf7cfe87433904b8023d3034c
Stanley <stanleyosazemwinde01@gmail.com>
2023-09-28 13:47:28 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - functionlike puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		putchar('\n');
+}
