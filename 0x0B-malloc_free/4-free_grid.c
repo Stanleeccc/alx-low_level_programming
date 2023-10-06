39f58b9b3dd621e19dc0c2655cd64046afdb41d9
Stanley <stanleyosazemwinde01@gmail.com>
2023-10-05 17:50:05 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -6,7 +6,6 @@
  * free_grid - a function that frees a 2 dimensional grid
  * @grid: represents the 2 dimensional grid
  * @height: represents the height dimension of the grid
- * @Description: represents the free memory of the grid
  * Return: nothing
  *
  */
